59fd8a1a9a1449deae00883ccc659cd129641d44
zrll <zhengruilin0@126.com>
2023-01-01 15:16:12 +0800

    新增：登录记录

--- src/user/login.rs
+++ src/user/login.rs
@@ -1,8 +1,10 @@
 use base64::encode;
-use mongodb::bson::{doc};
+use mongodb::bson::{doc, Document};
+use mongodb::Collection;
 use scrypt::{Params, scrypt};
 use crate::MONGODB;
 use crate::user::UserInfo;
+use chrono::{Local};
 
 impl UserInfo {
     fn check_password(&self, password: String) -> bool {
@@ -13,9 +15,13 @@ impl UserInfo {
     fn get_token(&self) -> String {
         todo!()
     }
+    async fn log_login(&self, ip: String, login_type: String) {
+        let collection: &Collection<Document> = &unsafe {MONGODB.as_ref()}.unwrap().collection("login_logs");
+        collection.insert_one(doc! {"username": &self.username, "time": &Local::now().format("%Y-%m-%d %H:%M:%S").to_string(), "type": login_type, "ip": ip}, None).await.unwrap();
+    }
 }
 
-pub async fn login_password(username: String, password: String) -> Result<String, String> {
+pub async fn login_password(username: String, password: String, ip: String) -> Result<String, String> {
     let collect = &unsafe { MONGODB.as_ref() }.unwrap().collection("users");
     let user: UserInfo = match collect.find_one(doc! {"username": &username}, None).await {
         Ok(Some(a)) => a,
@@ -29,5 +35,6 @@ pub async fn login_password(username: String, password: String) -> Result<String
         return Err("Wrong password".to_string());
     }
 
+    user.log_login(ip, "password".to_string()).await;
     Ok(user.get_token())
 }
